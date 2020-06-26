59939cb23776f1b70dd522a158201f43ff76bd77
dorsamoha <dorsamoha@gmail.com>
2020-06-26 15:35:58 +0300

    Updates

---
+++ precache-manifest.44fc9d0c1f1e5f21e5170e988d7f77be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9377b4bdbaf724bf88bd1a1650e42480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d252a83c154cece7a47c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "37a9c22eaac5385345bb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d252a83c154cece7a47c",
+    "url": "/static/js/2.2205109e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2205109e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a9c22eaac5385345bb",
+    "url": "/static/js/main.8c48791d.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
